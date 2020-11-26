584c464e625e5b98acb51fd2e6b46331a378eef6
YvesKalume <yvkalume@gmail.com>
2020-11-26 14:45:49 +0200

    add bottom nav view

--- app/src/main/java/com/yvkalume/dcplus/MainActivity.kt
+++ app/src/main/java/com/yvkalume/dcplus/MainActivity.kt
@@ -2,10 +2,26 @@ package com.yvkalume.dcplus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.viewbinding.library.activity.viewBinding
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.yvkalume.dcplus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by viewBinding()
+    private val navController by lazy {
+        Navigation.findNavController(this,R.id.fragment)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setUpNavigation()
+
+    }
+
+    private fun setUpNavigation() {
+        binding.bottomNavigationView.let {
+            NavigationUI.setupWithNavController(it,navController)
+        }
     }
 }
