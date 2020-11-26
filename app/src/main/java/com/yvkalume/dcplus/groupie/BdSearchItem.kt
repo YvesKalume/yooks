0ae08db177c2d6cce3234b4c7d18a9b8ace125ce
YvesKalume <yvkalume@gmail.com>
2020-11-26 19:01:34 +0200

    fix item bd_search

--- app/src/main/java/com/yvkalume/dcplus/groupie/BdSearchItem.kt
+++ app/src/main/java/com/yvkalume/dcplus/groupie/BdSearchItem.kt
@@ -4,14 +4,15 @@ import android.view.View
 import com.xwray.groupie.viewbinding.BindableItem
 import com.yvkalume.dcplus.R
 import com.yvkalume.dcplus.databinding.ItemBdSearchBinding
+import com.yvkalume.model.entity.Episode
 import com.yvkalume.model.entity.Serie
 
-class BdSearchItem(private val serie: Serie) : BindableItem<ItemBdSearchBinding>() {
+class BdSearchItem(private val bd: Episode) : BindableItem<ItemBdSearchBinding>() {
     override fun bind(viewBinding: ItemBdSearchBinding, position: Int) {
-        viewBinding.title.text = serie.title
+        viewBinding.title.text = bd.title
     }
 
-    override fun getLayout(): Int = R.layout.item_bd
+    override fun getLayout(): Int = R.layout.item_bd_search
 
     override fun initializeViewBinding(view: View): ItemBdSearchBinding =
         ItemBdSearchBinding.bind(view)
