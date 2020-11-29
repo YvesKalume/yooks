f223932bd9d757fcea16fcc6c97cb9f64c643a36
YvesKalume <yvkalume@gmail.com>
2020-11-29 12:13:49 +0200

    swipable favorites items

--- app/src/main/java/com/yvkalume/dcplus/ui/favorites/FavoriteFragment.kt
+++ app/src/main/java/com/yvkalume/dcplus/ui/favorites/FavoriteFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.viewbinding.library.fragment.viewBinding
+import android.widget.Toast
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.yvkalume.dcplus.R
@@ -17,13 +20,37 @@ import com.yvkalume.model.entity.Episode
 class FavoriteFragment : Fragment(R.layout.fragment_favorite) {
 
     private val binding: FragmentFavoriteBinding by viewBinding()
+    private val recyclerView by lazy { binding.recyclerView }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.recyclerView.adapter = favoriteAdapter
+        setUpRecyclerview()
         populateFavorite(getEpisodes())
     }
 
     private val favoriteAdapter = GroupAdapter<GroupieViewHolder>()
+
+    fun setUpRecyclerview() {
+        recyclerView.adapter = favoriteAdapter
+        val swipeCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val item = favoriteAdapter.getItem(viewHolder.adapterPosition) as BdHorizontalItem
+                Toast.makeText(context, item.episode.title,Toast.LENGTH_SHORT).show()
+                favoriteAdapter.notifyItemRemoved(viewHolder.adapterPosition)
+                //TODO : put deletetion confirmation dialogbox
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(swipeCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+    }
 
     private fun populateFavorite(episodes: List<Episode>) {
         val episodesItems = episodes.map { BdHorizontalItem(it) }
